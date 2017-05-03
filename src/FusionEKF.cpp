0cb154e0b0816ec9b4f0c20d6ebda25a68b20e83
Aziz Hararwala <ahararwala@pillartechnology.com>
2017-05-02 20:40:28 -0400

    Aziz: Uncomment cout for final submit

--- src/FusionEKF.cpp
+++ src/FusionEKF.cpp
@@ -87,7 +87,7 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
           * Remember: you'll need to convert radar from polar to cartesian coordinates.
         */
         // first measurement
-//        cout << "EKF: " << endl;
+        cout << "EKF: " << endl;
         ekf_.x_ = VectorXd(4);
         ekf_.x_ << 1, 1, 1, 1;
         if (measurement_pack.sensor_type_ == MeasurementPackage::RADAR) {
@@ -150,8 +150,8 @@ void FusionEKF::ProcessMeasurement(const MeasurementPackage &measurement_pack) {
     }
 
     // print the output
-//    cout << "x_ = " << ekf_.x_ << endl;
-//    cout << "P_ = " << ekf_.P_ << endl;
+    cout << "x_ = " << ekf_.x_ << endl;
+    cout << "P_ = " << ekf_.P_ << endl;
 }
 /**
    * Update the state transition matrix F according to the new elapsed time.
